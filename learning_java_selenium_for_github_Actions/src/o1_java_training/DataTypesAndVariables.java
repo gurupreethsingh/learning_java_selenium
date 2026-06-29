8ced78d4833ece75c4f19d3f6a46e11a10cbfb7a
igurupreeth <igurupreeth@gmail.com>
2026-06-29 11:26:23 +0530

    java learning project stated

---
+++ learning_java_selenium_for_github_Actions/src/o1_java_training/DataTypesAndVariables.java
@@ -0,0 +1,24 @@
+package o1_java_training;
+
+public class DataTypesAndVariables {
+
+	public static void main(String[] args)
+	{
+		byte b1 = 1; 
+		short s1 = 1; 
+		char ch = 'a';
+		int num1 = 12; 
+		float f1 = 3.4f;   // double
+		long num2 = 7658765876587657865l;
+		double d1 = 765765.8976876;
+		String s2 = "hello world";
+		System.out.println(b1);
+		System.out.println(s1);
+		System.out.println(ch);
+		System.out.println(f1);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(d1);
+		System.out.println(s2);
+	}
+}
