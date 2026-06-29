74bccf6566bc42f5579215a2a0ca7369d4cbcbfe
anjan121 <anjansa35@gmail.com>
2026-06-29 12:34:16 +0530

    Changed the function name

--- learning_java_selenium_for_github_Actions/src/o1_java_training/FunctionsPractice.java
+++ learning_java_selenium_for_github_Actions/src/o1_java_training/FunctionsPractice.java
@@ -2,13 +2,13 @@ package o1_java_training;
 
 public class FunctionsPractice 
 {
-	public static int add(int a, int b)
+	public static int addition(int a, int b)
 	{
 		return (a+b); 
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("the sum of two numbers is " + add(22, 33));
+		System.out.println("the sum of two numbers is " + addition(22, 33));
 	}
 
 }
