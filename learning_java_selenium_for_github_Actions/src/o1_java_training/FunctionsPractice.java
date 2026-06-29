5b26648501220d7ae1678258f863f04617862083
igurupreeth <igurupreeth@gmail.com>
2026-06-29 12:32:15 +0530

    functions class has been added ;

---
+++ learning_java_selenium_for_github_Actions/src/o1_java_training/FunctionsPractice.java
@@ -0,0 +1,14 @@
+package o1_java_training;
+
+public class FunctionsPractice 
+{
+	public static int add(int a, int b)
+	{
+		return (a+b); 
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("the sum of two numbers is " + add(22, 33));
+	}
+
+}
