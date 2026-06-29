cae0deec2b7c8577bf3a872b27bae636b1454123
anjan121 <anjansa35@gmail.com>
2026-06-29 12:24:00 +0530

    Added a line

--- learning_java_selenium_for_github_Actions/src/o1_java_training/DataTypesAndVariables.java
+++ learning_java_selenium_for_github_Actions/src/o1_java_training/DataTypesAndVariables.java
@@ -6,35 +6,30 @@ public class DataTypesAndVariables {
 	{
 		byte b1 = 1; 
 		short s1 = 1; 
-<<<<<<< HEAD
+
 		char c = 'S';
-		int num1 = 12; 
-		float f1 = 3.4f;   // double
-		long num2 = 765876587655l;
-		double d1 = 765765.89;
-		String s2 = "hello world - Java Learning";
-=======
+		
+
 		char ch2 = 'a';
 		int num1 = 12; 
 		float f1 = 3.4f;   // double
 		long num2 = 7658765876587657865l;
 		double d1 = 765765.8976876;
 		String s2 = "hello world Gurupreeth";
->>>>>>> ed0ee4d446f9e23b43f9128470bc8eab757d0697
+		String s4 = "Hello world Anjan";
 		String s3="shilpa";
 		char ch1[]=s3.toCharArray();
 		System.out.println(b1);
 		System.out.println(s1);
-<<<<<<< HEAD
+
 		System.out.println(c);
-=======
 		System.out.println(ch2);
->>>>>>> ed0ee4d446f9e23b43f9128470bc8eab757d0697
 		System.out.println(f1);
 		System.out.println(num1);
 		System.out.println(num2);
 		System.out.println(d1);
 		System.out.println(s2);
+		System.out.println(s4);
 		System.out.println(s2.toUpperCase());
 		System.out.println(s3.toUpperCase());
 
