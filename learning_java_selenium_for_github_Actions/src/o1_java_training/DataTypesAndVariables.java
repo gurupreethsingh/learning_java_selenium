85e6044bac2788de3a1aa423649834e5bd0eaabd
anjan121 <anjansa35@gmail.com>
2026-06-29 12:14:52 +0530

    Changed or alter the code

--- learning_java_selenium_for_github_Actions/src/o1_java_training/DataTypesAndVariables.java
+++ learning_java_selenium_for_github_Actions/src/o1_java_training/DataTypesAndVariables.java
@@ -6,16 +6,16 @@ public class DataTypesAndVariables {
 	{
 		byte b1 = 1; 
 		short s1 = 1; 
-		char ch = 'a';
+		char c = 'S';
 		int num1 = 12; 
 		float f1 = 3.4f;   // double
-		long num2 = 7658765876587657865l;
-		double d1 = 765765.8976876;
-		String s2 = "hello world";
+		long num2 = 765876587655l;
+		double d1 = 765765.89;
+		String s2 = "hello world - Java Learning";
 		String s3="shilpa";
 		System.out.println(b1);
 		System.out.println(s1);
-		System.out.println(ch);
+		System.out.println(c);
 		System.out.println(f1);
 		System.out.println(num1);
 		System.out.println(num2);
