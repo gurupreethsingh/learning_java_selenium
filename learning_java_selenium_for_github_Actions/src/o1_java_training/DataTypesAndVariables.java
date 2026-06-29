ed0ee4d446f9e23b43f9128470bc8eab757d0697
igurupreeth <igurupreeth@gmail.com>
2026-06-29 12:14:57 +0530

    updated code for helloworld

--- learning_java_selenium_for_github_Actions/src/o1_java_training/DataTypesAndVariables.java
+++ learning_java_selenium_for_github_Actions/src/o1_java_training/DataTypesAndVariables.java
@@ -6,17 +6,17 @@ public class DataTypesAndVariables {
 	{
 		byte b1 = 1; 
 		short s1 = 1; 
-		char ch = 'a';
+		char ch2 = 'a';
 		int num1 = 12; 
 		float f1 = 3.4f;   // double
 		long num2 = 7658765876587657865l;
 		double d1 = 765765.8976876;
-		String s2 = "hello world java";
+		String s2 = "hello world Gurupreeth";
 		String s3="shilpa";
 		char ch1[]=s3.toCharArray();
 		System.out.println(b1);
 		System.out.println(s1);
-		System.out.println(ch1);
+		System.out.println(ch2);
 		System.out.println(f1);
 		System.out.println(num1);
 		System.out.println(num2);
@@ -24,7 +24,6 @@ public class DataTypesAndVariables {
 		System.out.println(s2);
 		System.out.println(s2.toUpperCase());
 		System.out.println(s3.toUpperCase());
-		System.out.println(ch1.length);
 
 	}
 }
